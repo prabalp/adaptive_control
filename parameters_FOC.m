c61ed87d3e5b104e13ca730325009b1cf38f8c8d
mech-guy <60960925+mech-guy@users.noreply.github.com>
2021-08-29 11:34:20 +0530

    Field Oriented Control LIM

---
+++ parameters_FOC.m
@@ -0,0 +1,25 @@
+clear 
+
+Rs = 13.2;         % Resistance of stator
+Rr = 11.78;        % Resistance of rotor
+Ls = 0.42;         % Inductance of stator
+Lr = 0.42;         % Inductance of rotor
+Lm = 0.4;          % Mutual inductance
+Tp = 0.0465;       % Pole pitch
+t = Tp;
+M = 4.775;          % Mass of moving part
+P = 4;              % number of pole pairs
+B = 53;             % viscous friction
+lambda_r = 0.056;    %
+% = 1              % length of primary (in m)
+D = 0.25;            % length of linor/secondary (in m)
+
+% L_ls, L_lr leakage inductances of stator and rotor
+Lls = 0.005;
+Llr = 0.005;
+
+
+% W_e   primary, frequency
+% W_r   secondary frequency 
+% W_sl  slip electrical frequency
+
